b7325e1aef7bce3d267a00692a8086135293c863
João Périco <jeperico@terra.com.br>
2024-11-08 12:31:05 -0300

    [fix] adjust header script link

--- adm/adm-new/adm-new.js
+++ adm/adm-new/adm-new.js
@@ -4,29 +4,6 @@ var overlay = document.getElementById("overlay")
 var planet = document.getElementById("planet")
 
 
-var showing = -1
-
-function collapse(){
-    showing *= -1;
-
-    if (showing == 1){
-        sideBar.style.left = "0%"; 
-        overlay.style.opacity = "75%"; 
-        overlay.style.visibility = "visible" 
-        planet.style.left = "100vw"
-        planet.style.top = "100vh"
-        
-    }
-    else{
-        sideBar.style.left = "-80%"; 
-        overlay.style.opacity = "0%"; 
-        overlay.style.visibility = "hidden" 
-        planet.style.left = "200vw"
-        planet.style.top = "200vh"
-    };
-};
-
-
 function abrirPopup(tipo) {
     
     document.querySelector('.overlay').style.display = 'flex';
