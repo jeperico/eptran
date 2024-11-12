25ce096d02898fb44dcf0d0687d0675420deda60
caiomichels <caio.michels.f@gmail.com>
2024-11-12 20:38:15 -0300

    finish post carousel

--- assets/scripts/swiper.js
+++ assets/scripts/swiper.js
@@ -1,24 +1,36 @@
-import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
+import Swiper from "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs";
 
+const slider = new Swiper(".slider .swiper", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: true,
+    speed: 500,
 
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
+    // If we need pagination
+    pagination: {
+        el: ".swiper-pagination",
+    },
 
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
+    // Navigation arrows
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+    // And if we need scrollbar
+    scrollbar: {
+        el: ".swiper-scrollbar",
+    },
 
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
+    autoplay: {
+        delay: 5000,
+    },
+});
+
+const postCarousel = new Swiper(".post-carousel .swiper", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: true,
+    spaceBetween: 40,
+    slidesPerView: 2,
 });
